d7e62fa5f1fd0804f985f9d4564724a3b560dfea
Abhijeet <43548725+Abhijeetkumar96@users.noreply.github.com>
2024-02-18 13:27:47 +0530

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update to the directory containing .mtx files
-DIRECTORY="/home/datasets/"
+DIRECTORY="/home/datasets/mtx/"
 
 # Loop through all .mtx files in the specified directory
 for file in "$DIRECTORY"*.mtx; do
